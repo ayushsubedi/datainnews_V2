9e64e4cb99802c0661b09e6f876480cb5cd36a9f
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-30 18:50:05 +0545

    resolve #35

--- datainnews_v2/routes.py
+++ datainnews_v2/routes.py
@@ -1,11 +1,17 @@
 
 from datainnews_v2 import application
-from flask import request
-
+from flask import request, render_template
+import pandas as pd
 
 @application.route('/')
 def index():
-    return 'future home of news app'
+    df = pd.read_csv("datainnews_v2/static/csvs/NepaliTimes_analysis.csv", parse_dates=['created_at'], usecols=['id', 'created_at', 'urls'])
+    content = {
+        "table" : df.tail().to_html(classes="table"),
+        "size" : df.shape[0],
+        "last_updated" : df.created_at.max()
+    }
+    return render_template("index.html", **content)
 
 @application.route('/nepalitimes')
 def nepalitimes():
