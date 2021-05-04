89693d1bfafde0bb8aecc91f37f4dbd8d20f4ec1
Ayush Subedi <ayush.subedi@gmail.com>
2021-05-04 14:26:22 +0545

    add flake8 to the project

--- datainnews_v2/routes.py
+++ datainnews_v2/routes.py
@@ -3,17 +3,22 @@ from datainnews_v2 import application
 from flask import request, render_template
 import pandas as pd
 
+
 @application.route('/')
 def index():
-    df = pd.read_csv("datainnews_v2/static/csvs/NepaliTimes_analysis.csv", parse_dates=['created_at'], usecols=['id', 'created_at', 'urls'])
+    df = pd.read_csv(
+        "datainnews_v2/static/csvs/NepaliTimes_analysis.csv",
+        parse_dates=['created_at'],
+        usecols=['id', 'created_at', 'urls'])
     content = {
-        "table" : df.tail().to_html(classes="table"),
-        "size" : df.shape[0],
-        "last_updated" : df.created_at.max()
+        "table": df.tail().to_html(classes="table"),
+        "size": df.shape[0],
+        "last_updated": df.created_at.max()
     }
     return render_template("index.html", **content)
+
 
 @application.route('/nepalitimes')
 def nepalitimes():
     newspaper = request.endpoint
-    return request.endpoint
+    return newspaper
