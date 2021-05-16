399b0bb62bd6792e4392b24166a3e262c8ed8b44
Ayush Subedi <ayush.subedi@gmail.com>
2021-05-16 15:05:10 +0545

    resolve #62

--- datainnews_v2/routes.py
+++ datainnews_v2/routes.py
@@ -42,10 +42,30 @@ def index():
               {'name': 'Total Articles', 'data': df['News Articles'
               ].tolist()}]
     chart2 = (df['Level 1'] / df['News Articles'] * 100).tolist()
+    chart3 = [{'name': 'Level 2', 'data': df['Level 2'].tolist()},
+              {'name': 'Total Articles', 'data': df['News Articles'
+              ].tolist()}]
+    chart4 = (df['Level 2'] / df['News Articles'] * 100).tolist()
+
+    chart5 = [{'name': 'Level 3', 'data': df['Level 3'].tolist()},
+              {'name': 'Total Articles', 'data': df['News Articles'
+              ].tolist()}]
+    chart6 = (df['Level 3'] / df['News Articles'] * 100).tolist()
+
 
     level1 = df[['Newspaper', 'News Articles', 'Level 1', 'Level 1 %']]
     column_names_level1 = level1.columns.values
     row_data_level1 = list(level1.values.tolist())
+    
+    level2 = df[['Newspaper', 'Level 2', 'Filtered Articles',
+                'News Articles', 'Level 2 %']]
+    column_names_level2 = level2.columns.values
+    row_data_level2 = list(level2.values.tolist())
+
+    level3 = df[['Newspaper', 'Level 3', 'Filtered Articles',
+                'News Articles', 'Level 3 %']]
+    column_names_level3 = level3.columns.values
+    row_data_level3 = list(level3.values.tolist())
 
     newspapers = df['Newspaper'].tolist()
     
@@ -54,7 +74,15 @@ def index():
         'newspapers':newspapers,
         'chart1':chart1,
         'chart2':chart2,
+        'chart3':chart3,
+        'chart4':chart4,
+        'chart5':chart5,
+        'chart6':chart6,
         'column_names_level1': column_names_level1,
         'row_data_level1': row_data_level1,
+        'column_names_level2': column_names_level2,
+        'row_data_level2': row_data_level2,
+        'column_names_level3': column_names_level3,
+        'row_data_level3': row_data_level3,
     }
     return render_template("index.html", **content) 
