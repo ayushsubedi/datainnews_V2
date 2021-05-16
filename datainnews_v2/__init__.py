89a6759939e1f47e720316a8448168db8fe550c4
Ayush Subedi <ayush.subedi@gmail.com>
2021-05-16 18:30:34 +0545

    update requirements

--- datainnews_v2/__init__.py
+++ datainnews_v2/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from os import environ
 from os.path import join, dirname
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-stop_words = set(stopwords.words('english'))
+
 
 application = Flask(__name__)
 
